2a86816b3e666484eed7e6b6c24fcc3d2d672d6e
Emily Aquin <eaquin1@gmail.com>
2020-06-17 17:40:00 -0500

    added reset button

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,52 +1,62 @@
-import React, {useState} from "react"
-import "./EightBall.css"
+import React, { useState } from "react";
+import "./EightBall.css";
+
 function choice(arr) {
     const randomIdx = Math.floor(Math.random() * arr.length);
     return arr[randomIdx]
 }
 
 function EightBall(props) {
-    const [prediction, setPrediction] = useState("Think of a Question")
-    const [color, setColor] = useState("black")
+    
+    const restart = () => {
+        setColor("black");
+        setPrediction("Think of a Question")
+    }
+    const [prediction, setPrediction] = useState("Think of a Question");
+    const [color, setColor] = useState("black");
     function handleClick() {
-        const {msg, color} = choice(props.choices)
-        setPrediction(msg)
-        setColor(color)
+        const { msg, color } = choice(props.choices);
+        setPrediction(msg);
+        setColor(color);
     }
 
     return (
-    <div onClick={handleClick} 
-        className="EightBall"
-        style={{ backgroundColor: color }}>
-        <p className="EightBall-text">{prediction}</p>
-    </div>
-    )
+        <>
+            <div
+                onClick={handleClick}
+                className="EightBall"
+                style={{ backgroundColor: color }}
+            >
+                <p className="EightBall-text">{prediction}</p>
+            </div>
+            <button onClick={restart}>Reset</button>
+        </>
+    );
 }
-
 
 EightBall.defaultProps = {
     choices: [
-    { msg: "It is certain.", color: "green" },
-    { msg: "It is decidedly so.", color: "green" },
-    { msg: "Without a doubt.", color: "green" },
-    { msg: "Yes - definitely.", color: "green" },
-    { msg: "You may rely on it.", color: "green" },
-    { msg: "As I see it, yes.", color: "green" },
-    { msg: "Most likely.", color: "green" },
-    { msg: "Outlook good.", color: "green" },
-    { msg: "Yes.", color: "green" },
-    { msg: "Signs point to yes.", color: "goldenrod" },
-    { msg: "Reply hazy, try again.", color: "goldenrod" },
-    { msg: "Ask again later.", color: "goldenrod" },
-    { msg: "Better not tell you now.", color: "goldenrod" },
-    { msg: "Cannot predict now.", color: "goldenrod" },
-    { msg: "Concentrate and ask again.", color: "goldenrod" },
-    { msg: "Don't count on it.", color: "red" },
-    { msg: "My reply is no.", color: "red" },
-    { msg: "My sources say no.", color: "red" },
-    { msg: "Outlook not so good.", color: "red" },
-    { msg: "Very doubtful.", color: "red" },
-  ]
-}
+        { msg: "It is certain.", color: "green" },
+        { msg: "It is decidedly so.", color: "green" },
+        { msg: "Without a doubt.", color: "green" },
+        { msg: "Yes - definitely.", color: "green" },
+        { msg: "You may rely on it.", color: "green" },
+        { msg: "As I see it, yes.", color: "green" },
+        { msg: "Most likely.", color: "green" },
+        { msg: "Outlook good.", color: "green" },
+        { msg: "Yes.", color: "green" },
+        { msg: "Signs point to yes.", color: "goldenrod" },
+        { msg: "Reply hazy, try again.", color: "goldenrod" },
+        { msg: "Ask again later.", color: "goldenrod" },
+        { msg: "Better not tell you now.", color: "goldenrod" },
+        { msg: "Cannot predict now.", color: "goldenrod" },
+        { msg: "Concentrate and ask again.", color: "goldenrod" },
+        { msg: "Don't count on it.", color: "red" },
+        { msg: "My reply is no.", color: "red" },
+        { msg: "My sources say no.", color: "red" },
+        { msg: "Outlook not so good.", color: "red" },
+        { msg: "Very doubtful.", color: "red" },
+    ],
+};
 
-  export default EightBall
+export default EightBall;
